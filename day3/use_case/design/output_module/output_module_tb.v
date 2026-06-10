1efdcbc147bdffa19d5d2fb1ba610503a7d43199
rsabhijith <abhijithrs712@gmail.com>
2026-06-10 22:45:31 +0530

    Update output_module_tb.v

--- day3/use_case/design/output_module/output_module_tb.v
+++ day3/use_case/design/output_module/output_module_tb.v
@@ -20,38 +20,55 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module output_module_tb(
+module out_module_tb;
 
-    );
-    reg [7:0]d_in_tb;
-    reg clk_tb;
-    wire [7:0]d_out_tb;
-    output_module dut (d_in_tb,clk_tb,d_out_tb);
-    always #5 clk_tb=~clk_tb;
-    initial
-    begin
-    {d_in_tb,clk_tb}=0;
-    end
-    initial
-    begin
-    #10 
-    d_in_tb = 8'h10;
-    #10 
-    d_in_tb = 8'h20;
-    #10 
-    d_in_tb = 8'h30;
-    #10 
-    d_in_tb = 8'h40;
-    #10 
-    d_in_tb = 8'h50;
-    #10 
-    d_in_tb = 8'h60;
-    #10 
-    d_in_tb = 8'h70;
-    #10 
-    d_in_tb = 8'h80;
+reg clk_tb;
+reg rst_tb;
+reg [7:0] din_tb;
 
-    end
-    
+wire rd_enb_tb;
+wire [7:0] dout_tb;
+
+out_module dut(
+    clk_tb,
+    rst_tb,
+    din_tb,
+    rd_enb_tb,
+    dout_tb
+);
+
+always #5 clk_tb = ~clk_tb;
+
+initial
+begin
+    {clk_tb,rst_tb,din_tb} = 0;
+end
+
+initial
+begin
+    rst_tb = 1;
+    #10
+    rst_tb = 0;
+
+    #10 
+    din_tb = 8'h10;
+    #10 
+    din_tb = 8'h20;
+    #10 
+    din_tb = 8'h30;
+    #10 
+    din_tb = 8'h40;
+    #10 
+    din_tb = 8'h50;
+    #10 
+    din_tb = 8'h60;
+    #10 
+    din_tb = 8'h70;
+    #10 
+    din_tb = 8'h80;
+
+    #100
+    $finish;
+end
+
 endmodule
-
