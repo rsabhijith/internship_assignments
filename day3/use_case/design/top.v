913d689454cbd4c40786db3eabe1226bec730ef0
rsabhijith <abhijithrs712@gmail.com>
2026-06-10 22:53:17 +0530

    Update top.v

--- day3/use_case/design/top.v
+++ day3/use_case/design/top.v
@@ -24,7 +24,7 @@ module top_module(input clk,rst,input [7:0] s_in,output [7:0] d_out
   wire [7:0] s_out, data_out;
 wire rd_enb, full, empty;
 face_module dut(clk,s_in,s_out);
-  fifo ff1(clk,rst,1'b1,rd_enb,s_out,full,empty,data_out);
-  out_module om1(clk,rst,data_out,rd_enb,d_out);
+fifo ff1(clk,rst,1'b1,rd_enb,s_out,full,empty,data_out);
+output_module om1(clk,rst,data_out,rd_enb,d_out);
 
 endmodule
