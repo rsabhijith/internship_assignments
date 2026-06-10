23636460aba21782087e6f027111f12a650ea3f5
rsabhijith <abhijithrs712@gmail.com>
2026-06-10 23:10:45 +0530

    Update output_module.v

--- day3/use_case/design/output_module/output_module.v
+++ day3/use_case/design/output_module/output_module.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 9:35:43
+// Design Name: 
+// Module Name: output_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module output_module(
     input clk,
     input rst,
